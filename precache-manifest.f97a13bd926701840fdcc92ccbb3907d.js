709629c82c45a05524e441f699fad783597dbd28
dixan.santiesteban@directa24.com <dixan.santiesteban@directa24.com>
2020-07-31 14:24:00 -0300

    Updates

---
+++ precache-manifest.f97a13bd926701840fdcc92ccbb3907d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b696c5edd0407d7fb2254d4fdf31cf",
+    "url": "/full-panel/index.html"
+  },
+  {
+    "revision": "d9d173f5514a831e9603",
+    "url": "/full-panel/static/css/2.5361eff3.chunk.css"
+  },
+  {
+    "revision": "e52e87055766f9bbae03",
+    "url": "/full-panel/static/css/main.1acb609b.chunk.css"
+  },
+  {
+    "revision": "d9d173f5514a831e9603",
+    "url": "/full-panel/static/js/2.adb73155.chunk.js"
+  },
+  {
+    "revision": "5740b5df4485597e01f1024c8a6b9353",
+    "url": "/full-panel/static/js/2.adb73155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52e87055766f9bbae03",
+    "url": "/full-panel/static/js/main.5c85907e.chunk.js"
+  },
+  {
+    "revision": "fcdc1bea1f8dff81c549",
+    "url": "/full-panel/static/js/runtime-main.7aa64db6.js"
+  },
+  {
+    "revision": "ee8e12c956e365f0530b7a2bbb5e82a1",
+    "url": "/full-panel/static/media/bg-main.ee8e12c9.png"
+  },
+  {
+    "revision": "d4c6771dcc3244bfd73b44def4b6d770",
+    "url": "/full-panel/static/media/customerpanel.d4c6771d.png"
+  }
+]);
